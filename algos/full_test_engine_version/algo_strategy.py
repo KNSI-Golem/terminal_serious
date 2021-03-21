7338e13304ad906e42896483dafef4d587450385
Antoni Kowalczuk <a.kowalczuk@stud.elka.pw.edu.pl>
2021-03-21 16:06:44 +0100

    Implemented full test

---
+++ algos/full_test_engine_version/algo_strategy.py
@@ -0,0 +1,100 @@
+import gamelib
+import random
+import math
+import warnings
+from sys import maxsize
+import json
+
+
+"""
+Most of the algo code you write will be in this file unless you create new
+modules yourself. Start by modifying the 'on_turn' function.
+
+Advanced strategy tips: 
+
+  - You can analyze action frames by modifying on_action_frame function
+
+  - The GameState.map object can be manually manipulated to create hypothetical 
+  board states. Though, we recommended making a copy of the map to preserve 
+  the actual current map state.
+"""
+
+class AlgoStrategy(gamelib.AlgoCore):
+    def __init__(self):
+        super().__init__()
+        seed = random.randrange(maxsize)
+        random.seed(seed)
+
+    def on_game_start(self, config):
+        """ 
+        Read in config and perform any initial setup here 
+        """
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR
+        WALL = config["unitInformation"][0]["shorthand"]
+        SUPPORT = config["unitInformation"][1]["shorthand"]
+        TURRET = config["unitInformation"][2]["shorthand"] 
+        SCOUT = config["unitInformation"][3]["shorthand"]
+        DEMOLISHER = config["unitInformation"][4]["shorthand"]
+        INTERCEPTOR = config["unitInformation"][5]["shorthand"]
+        self.config = config
+
+    def on_turn(self, turn_state):
+        """
+        This function is called every turn with the game state wrapper as
+        an argument. The wrapper stores the state of the arena and has methods
+        for querying its state, allocating your current resources as planned
+        unit deployments, and transmitting your intended deployments to the
+        game engine.
+        """
+        game_state = gamelib.GameState(self.config, turn_state)
+        self.spawn(game_state)
+        game_state.submit_turn()
+
+    def spawn(self, game_state):
+        friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
+        # structure spawn loop
+        for _ in range(10):
+            x, y = random.randint(0, 28), random.randint(0, 28)
+            unit_type = random.choice([WALL, TURRET, SUPPORT])
+            try:
+                game_state.attempt_spawn(unit_type, (x, y), 1)
+            except:
+                pass
+
+        # units spawn loop
+        for _ in range(3):
+            location = random.choice(friendly_edges)
+            unit_type = random.choice([SCOUT, DEMOLISHER, INTERCEPTOR])
+            try:
+                game_state.attempt_spawn(unit_type, location)
+            except:
+                pass
+        
+        # upgrades loop
+        for _ in range(10):
+            x, y = random.randint(0, 28), random.randint(0, 28)
+            try:
+                game_state.attempt_upgrade((x, y))
+            except:
+                pass
+
+        # deletions loop
+        for _ in range(10):
+            x, y = random.randint(0, 28), random.randint(0, 28)
+            try:
+                game_state.attempt_remove((x, y))
+            except:
+                pass
+
+    def on_action_frame(self, turn_string):
+        """
+        This is the action frame of the game. This function could be called 
+        hundreds of times per turn and could slow the algo down so avoid putting slow code here.
+        Processing the action frames is complicated so we only suggest it if you have time and experience.
+        Full doc on format of a game frame at in json-docs.html in the root of the Starterkit.
+        """
+
+
+if __name__ == "__main__":
+    algo = AlgoStrategy()
+    algo.start()
